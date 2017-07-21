98ddb2658ae6daade3a520faf30a8cf6930d0dd5
Bryan Oliveira <bryan.oliveira@broxel.com>
2017-07-21 08:43:50 -0500

    Fixed all linter errors for dbUserHelper.js

--- helpers/dbUserHelper.js
+++ helpers/dbUserHelper.js
@@ -1,39 +1,38 @@
-const mongoose = require('mongoose')
-const User = require('../api/models/userModel')
+const User = require('../api/models/userModel');
 
 exports.find_all = (callback) => {
   User.find({}, (err, users) => {
-    callback(err, users)
-  })
-}
+    callback(err, users);
+  });
+};
 
 exports.save_user = (body, callback) => {
-  const newUser = new User(body)
+  const newUser = new User(body);
   newUser.save((err, user) => {
-    callback(err, user)
-  })
-}
+    callback(err, user);
+  });
+};
 
 exports.find_user = (userId, callback) => {
   User.findById(userId, (err, user) => {
-    callback(err, user)
-  })
-}
+    callback(err, user);
+  });
+};
 
 exports.update_user = (userId, body, callback) => {
   User.findOneAndUpdate({
-    _id: userId
+    _id: userId,
   }, body, {
-    new: true
+    new: true,
   }, (err, user) => {
-    callback(err, user)
-  })
-}
+    callback(err, user);
+  });
+};
 
 exports.delete_user = (userId, callback) => {
   User.remove({
-    _id: userId
-  }, function(err, user) {
-    callback(err, user)
-  })
-}
+    _id: userId,
+  }, (err, user) => {
+    callback(err, user);
+  });
+};
